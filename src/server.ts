101baa53779689a87e218d4b06e8af184d1d669e
ViniciusCharleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-02-24 01:29:53 -0300

    iniciando as rotas e o server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
-import express from 'express';
+import express, { response } from 'express';
+import { request } from 'http';
+import { send } from 'process';
 
 const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({message: "Hello World NLW04"});
+})
+
+app.post('/', (request, response) => {
+
+})
 
 app.listen(3333, () => {
     console.log("Server is running!!");
