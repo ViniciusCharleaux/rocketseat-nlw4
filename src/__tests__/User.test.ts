7ce891516cb95f2090226c225b891ffb26bdca7f
ViniciusCharleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-03-04 00:43:00 -0300

    Iniciando Testes Automatizados

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,9 @@
+import request from 'supertest';
+
+describe("Users", () => {
+    request(app).post("/users")
+    .send({
+        email: "user@example.com",
+        name: "User Example"
+    })
+})
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';  //importante vir primeiro nos imports
+import express from 'express';
+import './database';
+import { router } from './routes';
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+export { app };
